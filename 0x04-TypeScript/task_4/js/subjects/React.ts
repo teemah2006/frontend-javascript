b3daeef1e20e9024b7def96e5499dd465b4a5e89
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-14 16:04:14 +0100

    task 9 complete

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,17 @@
+/// <reference path="Teacher.ts" />
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingReact ?: number;
+    }
+    export class React extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for React';
+        }
+        getAvailableTeacher (): string {
+            if (!this.teacher || !this.teacher.experienceTeachingReact) {
+                return 'No available teacher';
+            }
+            return `Available Teacher: ${this.teacher.firstName}`;
+        }
+    }
+}
