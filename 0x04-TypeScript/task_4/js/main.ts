b709f39444909f34b1f1eebc352ff9e88320dcc2
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-14 17:36:32 +0100

    task 10 complete

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,3 +1,9 @@
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/Java.ts" />
+/// <reference path="subjects/React.ts" />
+/// <reference path="subjects/Teacher.ts" />
+
+import Teacher = Subjects.Teacher;
 import Cpp = Subjects.Cpp;
 import React = Subjects.React;
 import Java = Subjects.Java;
@@ -5,18 +11,18 @@ export const cpp = new Cpp();
 export const react = new React();
 export const java = new Java();
 
-export const cTeacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
+export const cTeacher: Teacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
 console.log('c++');
-cpp.setTeacher = cTeacher;
+cpp.teacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 console.log('Java');
-java.setTeacher = cTeacher;
+java.teacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
 console.log('React');
-react.setTeacher = cTeacher;
+react.teacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
