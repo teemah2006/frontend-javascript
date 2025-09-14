4b8c89e5eb34a041bde51c9fef49b0c05b188e9d
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-14 16:15:40 +0100

    task 10 complete

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,19 @@
+export const cpp = new Subjects.Cpp();
+export const react = new Subjects.React();
+export const java = new Subjects.Java();
+
+export const cTeacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
+console.log('c++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
